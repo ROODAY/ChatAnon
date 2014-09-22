178d2d54ca7732f77a81b3045240a8e244591b7c
ROODAY <rudytheninja@gmail.com>
2014-09-22 19:04:02 -0400

    update

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ SecretChatRoom
 ==============
 
 Please don't read me
+
+To do
+=====
+- [ ] Prettify
+- [ ] add random colors to different people
+- [ ] scroll to latest post
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
