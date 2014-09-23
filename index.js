00ea3a0cf2547ff38d6fa3832f09b952e7925983
ROODAY <rudytheninja@gmail.com>
2014-09-23 16:36:09 -0400

    removed hard to see colors

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ Colors.names = {
     aqua: "#00ffff",
     azure: "#f0ffff",
     beige: "#f5f5dc",
-    black: "#000000",
     blue: "#0000ff",
     brown: "#a52a2a",
     cyan: "#00ffff",
@@ -34,12 +33,6 @@ Colors.names = {
     green: "#008000",
     indigo: "#4b0082",
     khaki: "#f0e68c",
-    lightblue: "#add8e6",
-    lightcyan: "#e0ffff",
-    lightgreen: "#90ee90",
-    lightgrey: "#d3d3d3",
-    lightpink: "#ffb6c1",
-    lightyellow: "#ffffe0",
     lime: "#00ff00",
     magenta: "#ff00ff",
     maroon: "#800000",
@@ -49,10 +42,7 @@ Colors.names = {
     pink: "#ffc0cb",
     purple: "#800080",
     violet: "#800080",
-    red: "#ff0000",
-    silver: "#c0c0c0",
-    white: "#ffffff",
-    yellow: "#ffff00"
+    red: "#ff0000"
 };
 Colors.random = function() {
     var result;
