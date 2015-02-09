35814c9e714c33e8aa489998fd9451154b67916e
ROODAY <rudytheninja@gmail.com>
2015-02-09 17:17:00 -0500

    finished almost everything

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var markdown = require('marked');
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 var storage = require('node-persist');
+var util = require("util");
 var chatlog = {
     "ChatAnon": []
 };
@@ -12,13 +13,10 @@ var connectclients = [];
 Colors = {};
 Colors.names = {
     red: "#F44336",
-    pink: "#E91E63",
     purple: "#9C27B0",
     deeppurple: "#673AB7",
     indigo: "#3F51B5",
     blue: "#2196F3",
-    lightblue: "#03A9F4",
-    cyan: "#00BCD4",
     teal: "#009688",
     green: "#4CAF50",
     lightgreen: "#8BC34A",
@@ -57,10 +55,12 @@ io.on('connection', function(socket){
         delete roomList[client];
     });
     socket.emit("roomList", Object.keys(roomList));
-    socket.emit('localID', socket.id);
+    socket.emit('localID', socket.id, Colors.random());
     socket.emit('firstjoin', chatlog.ChatAnon);
     io.sockets.in("ChatAnon").emit('ding', true);
-    io.sockets.in("ChatAnon").emit('console chat message', "A user connected.");
+    io.sockets.emit('console chat message', "A user connected.");
+    io.sockets.in("ChatAnon").emit("updateClientNumber", Object.keys(io.sockets.adapter.rooms["ChatAnon"]).length);
+    io.sockets.in("ChatAnon").emit('console chat message', "A user entered the room.");
 
     socket.on("joinRoom", function(curRoom, nextRoom){
         socket.leave(curRoom);
@@ -69,22 +69,24 @@ io.on('connection', function(socket){
         connectclients.forEach(function(client){
             delete roomList[client];
         });
-        io.emit("roomList", Object.keys(roomList));
+        io.sockets.emit("roomList", Object.keys(roomList));
         if (!(nextRoom in chatlog)) {
             chatlog[nextRoom] = [];
         } else {
             socket.emit("firstjoin", chatlog[nextRoom]);
         }
-        io.sockets.in(curRoom).emit("updateClientNumber", io.sockets.adapter.rooms[curRoom].length);
-        io.sockets.in(nextRoom).emit("updateClientNumber", io.sockets.adapter.rooms[nextRoom].length);
-        io.sockets.in(curRoom).emit('ding', true);
-        io.sockets.in(curRoom).emit('console chat message', "A user disconnected.");
+        if (io.sockets.adapter.rooms[curRoom]) {
+            io.sockets.in(curRoom).emit("updateClientNumber", Object.keys(io.sockets.adapter.rooms[curRoom]).length);
+            io.sockets.in(curRoom).emit('ding', true);
+            io.sockets.in(curRoom).emit('console chat message', "A user left the room.");
+        }
+        io.sockets.in(nextRoom).emit("updateClientNumber", Object.keys(io.sockets.adapter.rooms[nextRoom]).length);
         io.sockets.in(nextRoom).emit('ding', true);
-        io.sockets.in(nextRoom).emit('console chat message', "A user connected.");
+        io.sockets.in(nextRoom).emit('console chat message', "A user entered the room.");
     });
 
     socket.on('disconnect',function(){
-        console.log('User Disconnected. ID: ' + socket.id);
+        io.sockets.emit('console chat message', "A user disconnected.");
     });
 
 	socket.on('chat message', function(text, color, room){
@@ -93,13 +95,13 @@ io.on('connection', function(socket){
 
     socket.on('pushli', function(text, color, room){
         if (text != undefined && text != null && text != "" && color != undefined && color != null && color != "") {
-            if (oldMessages.length < 51) {
+            if (chatlog[room].length < 51) {
                 var textobject = [text, color]
-                oldMessages.push(textobject);
+                chatlog[room].push(textobject);
             } else {
                 var textobject = [text, color]
-                oldMessages.splice(0,1);
-                oldMessages.push(textobject);
+                chatlog[room].splice(0,1);
+                chatlog[room].push(textobject);
             }
             storage.setItem('chatlog', chatlog);
         } 
@@ -116,5 +118,5 @@ setInterval(function(){
     connectclients.forEach(function(client){
         delete roomList[client];
     });
-    io.emit("roomList", Object.keys(roomList));
+    io.sockets.emit("roomList", Object.keys(roomList));
 }, 1000);
